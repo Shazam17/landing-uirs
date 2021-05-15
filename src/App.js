f7eeac1935b5b468bbaa9da365b7f88112c5a6d3
JohnPower <stkautoru@mail.ru>
2021-05-15 18:35:34 +0700

    init

--- src/App.js
+++ src/App.js
@@ -1,23 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
+import React, {useEffect, useRef} from "react";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div style={{width: '70%', height: '4000px', margin:'0 auto'}}>
+            TUSUR-ONLINE
+        </div>
     </div>
   );
 }
